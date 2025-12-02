09e322609fa7f3de4450108e6ca605a13d6c90dc
Ivan Nguyen <ivan.nguyen2@education.nsw.gov.au>
2025-12-02 13:01:24 +1100

    Update directory structure

---
+++ .database/myqueries.sql
@@ -0,0 +1,7 @@
+--CREATE TABLE Anime(Anime_id INTEGER NOT NULL PRIMARY KEY,Anime_name TEXT NOT NULL, Anime_Description TEXT NOT NULL);
+
+--INSERT INTO Anime(Anime_id,Anime_name,Anime_Description) 
+--VALUES (1,"One_piece","Pirate") , (2,"Naruto","Ninja") , (3,"Dragon_Ball_Z","Alien");
+
+SELECT * FROM Anime WHERE Anime_id = 1
+
